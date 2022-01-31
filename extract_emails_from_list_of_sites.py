71f5d98b987b52bae39fde7e74b6cbc8d55d459b
O <48214249+OlegKorn@users.noreply.github.com>
2022-01-31 19:57:22 +0800

    Create extract_emails_from_list_of_sites.py

---
+++ extract_emails_from_list_of_sites.py
@@ -0,0 +1,50 @@
+import requests
+import re
+from bs4 import BeautifulSoup as bs
+import logging
+
+
+FORMAT = '%(message)s'
+
+file = "G:/Desktop/ЭКОЛОГИЯ/schule.txt"
+path = "G:/Desktop/ЭКОЛОГИЯ/"
+
+emails = ""
+
+f = open(file, 'r').readlines()
+
+logging.basicConfig(
+    filename=f"{path}emails.log",
+    filemode='w+',
+    level=logging.INFO, 
+    format=FORMAT
+)
+
+for i in f:
+    site = re.search("\"(.*?)\"", i).group(1)
+
+    session = requests.Session()
+    try:
+        r = requests.get(site.strip())
+    except Exception:
+        print("===========")
+        print(f"{site} is down")
+        print("===========")
+        continue 
+
+    soup = bs(r.content, 'html.parser')
+
+    try:
+        mails = re.findall(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]+', soup.text)
+        if len(mails) > 0:
+            for mail in mails:
+                emails += mail + ", "
+
+            print("===========")
+            print(f"{site} ---------------------->>>>>>>>>>>>>>>>> {emails}")
+            print("===========\n")
+            logging.info(f"{emails}\n\n")
+
+    except Exception as e:
+        print(e)
+        continue
